7537e59d5701d831f56c14fffcbcc674ffae2441
Thanh Tan Le <thanhtan1105@gmail.com>
2024-05-17 12:44:16 +0700

    update init script

--- plugins/init_script.sql
+++ plugins/init_script.sql
@@ -1,23 +1,44 @@
-CREATE TABLE IF NOT EXISTS public.Customers (
-    CustomerID SERIAL PRIMARY KEY,
-    FirstName VARCHAR(40),
-    LastName VARCHAR(40),
-    Email VARCHAR(60)
+create table if not exists public.customers
+(
+    customerid serial
+        primary key,
+    firstname  varchar(40),
+    lastname   varchar(40),
+    email      varchar(60)
+        constraint customers_pk
+            unique
 );
 
-CREATE TABLE IF NOT EXISTS public.Accounts (
-    AccountID SERIAL PRIMARY KEY,
-    CustomerID INT,
-    Balance DECIMAL(10, 2),
-    AccountType VARCHAR(20),
-    FOREIGN KEY (CustomerID) REFERENCES Customers(CustomerID)
+alter table public.customers
+    owner to airflow;
+
+create table if not exists public.accounts
+(
+    account_id   integer default nextval('accounts_accountid_seq'::regclass) not null
+        primary key,
+    customerid   integer
+        constraint accounts_unique_customerid
+            unique
+        references public.customers,
+    balance      numeric(10, 2),
+    account_type varchar(20)
 );
 
-CREATE TABLE IF NOT EXISTS public.Transactions (
-    TransactionID SERIAL PRIMARY KEY,
-    AccountID INT,
-    Amount DECIMAL(10, 2),
-    TransactionType VARCHAR(20),
-    TransactionDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (AccountID) REFERENCES Accounts(AccountID)
+alter table public.accounts
+    owner to airflow;
+
+create table if not exists public.transactions
+(
+    transaction_id   integer   default nextval('transactions_transactionid_seq'::regclass) not null
+        primary key,
+    account_id       integer
+        constraint transactions_accountid_fkey
+            references public.accounts,
+    amount           numeric(10, 2),
+    transaction_type varchar(20),
+    transaction_date timestamp default CURRENT_TIMESTAMP
 );
+
+alter table public.transactions
+    owner to airflow;
+
